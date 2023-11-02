d404fde6211d818b87e8d1cc82e7759e404f8101
addison-schwamb <97481978+addison-schwamb@users.noreply.github.com>
2023-11-02 16:53:19 -0500

    Delete import_data.m

--- import_data.m
+++
@@ -1,8 +0,0 @@
-
-
-function [data,header] = import_data(file)
-    path = 'C:\Users\a.l.schwamb\Box\CA\';
-
-    header = pop_biosig(strcat(path,file),'importmex','on','importevent','off');
-    data = header.data;
-end
